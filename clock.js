d292d5daaa8bbc5c7941828bce4cb6e2e3ade84e
bejibay <aykayode@gmail.com>
2021-05-17 07:17:28 -0700

    two files added

---
+++ clock.js
@@ -0,0 +1,22 @@
+<script>
+var date, h, m. s, period;
+function showTime (){
+
+// create the date object
+date = new Date();
+h = date.getHours();
+m = date.getMonths();
+s = date.getSeconds();
+m  = control(m);
+s  = control(s);
+if(h<12){period="AM";return h;}
+if(h>12){period="PM" return h;}
+document.getElementById("display").InnerHTML= h+":"+m+":"+s+period;
+setTimeout(showTime, 500);
+
+}
+function control(t) {
+if(t<12){return "0"+t;}
+else{return t;}
+}
+</script>
